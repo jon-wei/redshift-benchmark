f7912aa6e26a2b57fad6eb19b2c99b7328a2ecc6
jon-wei <jon.wei@imply.io>
2016-12-01 23:46:10 -0800

    Only run groupby v2 benchmark

--- benchmark-druid.R
+++ benchmark-druid.R
@@ -231,20 +231,21 @@ group_by_shipmode_v2 <- function(datasource) {
   )
 }
 
-res1 <- microbenchmark(count_star_interval(datasource), times=n)
-res2 <- microbenchmark(sum_price(datasource), times=n)
-res3 <- microbenchmark(sum_all(datasource), times=n)
-res4 <- microbenchmark(sum_all_year(datasource), times=n)
-res5 <- microbenchmark(sum_all_filter(datasource), times=n)
-res6 <- microbenchmark(top_100_parts(datasource), times=n)
-res7 <- microbenchmark(top_100_parts_details(datasource), times=n)
-res8 <- microbenchmark(top_100_parts_filter(datasource), times=n)
-res9 <- microbenchmark(top_100_commitdate(datasource), times=n)
+#res1 <- microbenchmark(count_star_interval(datasource), times=n)
+#res2 <- microbenchmark(sum_price(datasource), times=n)
+#res3 <- microbenchmark(sum_all(datasource), times=n)
+#res4 <- microbenchmark(sum_all_year(datasource), times=n)
+#res5 <- microbenchmark(sum_all_filter(datasource), times=n)
+#res6 <- microbenchmark(top_100_parts(datasource), times=n)
+#res7 <- microbenchmark(top_100_parts_details(datasource), times=n)
+#res8 <- microbenchmark(top_100_parts_filter(datasource), times=n)
+#res9 <- microbenchmark(top_100_commitdate(datasource), times=n)
 # res10 <- microbenchmark(group_by_shipmode(datasource), times=n)
-# res11 <- microbenchmark(group_by_shipmode_v2(datasource), times=n)
+res11 <- microbenchmark(group_by_shipmode_v2(datasource), times=n)
 
 # results <- as.data.frame(rbind(res1, res2, res3, res4, res5, res6, res7, res8, res9, res10, res11))
-results <- as.data.frame(rbind(res1, res2, res3, res4, res5, res6, res7, res8, res9))
+#results <- as.data.frame(rbind(res1, res2, res3, res4, res5, res6, res7, res8, res9))
+results <- as.data.frame(rbind(res11))
 results$time <- results$time / 1e9
 results$query <- as.character(sub("\\(.*\\)", replacement="", results$expr))
 druid <- results[c("query", "time")]
